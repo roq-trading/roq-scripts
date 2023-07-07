f3c428142c6600b47713721118f7c4a1723bad25
thraneh <thraneh@gmail.com>
2023-07-07 07:13:33 +0200

    copy-paste + small rewrite

---
+++ templates/flags/flags.cpp
@@ -0,0 +1,100 @@
+{% include 'autogen' %}
+
+{% include 'include' %}
+
+#include <absl/flags/declare.h>
+#include <absl/flags/flag.h>
+
+#include <string>
+
+#include "roq/core/flags/flags.hpp"
+
+#include "roq/core/charconv.hpp"
+
+using namespace std::literals;
+using namespace std::chrono_literals;  // NOLINT
+
+{% for value in values %}
+{%- if not value.is_external %}
+ABSL_FLAG(  //
+{%- if value.has_validator %}
+    {{ value.validator }},
+{%- else %}
+{%- if value.type == 'std::vector<uint16_t>' %}
+    std::vector<std::string>,
+{%- else %}
+    {{ value.type }},
+{%- endif %}
+{%- endif %}
+    {{ prefix }}{{ value.flag_name }},
+{%- if value.is_string or value.is_enum %}
+    { {{ value.default_raw }} },
+{%- else %}
+    {{ value.default_raw }},
+{%- endif %}
+    "{{ value.description }}"s);
+{%- endif %}
+{% endfor %}
+
+// external
+
+{% for value in values %}
+{%- if value.is_external %}
+{%- if value.has_validator %}
+ABSL_DECLARE_FLAG({{ value.validator }}, {{ value.flag_name }});
+{%- else %}
+ABSL_DECLARE_FLAG({{ value.type }}, {{ value.flag_name }});
+{%- endif %}
+{%- endif %}
+{%- endfor %}
+
+{% include 'namespace_begin' %}
+
+{% for value in values %}
+{%- if value.is_string %}
+  std::string_view {{ name }}::{{ prefix }}{{ value.name }}() {
+{%- else %}
+{%- if value.is_pod_or_std %}
+  {{ value.type }} {{ name }}::{{ prefix }}{{ value.name }}() {
+{%- else %}
+  const {{ value.type }} &{{ name }}::{{ prefix }}{{ value.name }}() {
+{%- endif %}
+{%- endif %}
+{%- if value.type == 'std::chrono::nanoseconds' and value.validator == 'roq::core::flags::TimePeriod' %}
+  static const {{ value.type }} result{absl::ToChronoNanoseconds(absl::GetFlag(FLAGS_{{ prefix }}{{ value.flag_name }}))};
+{%- else %}
+{%- if value.type == 'std::vector<uint16_t>' %}
+  auto parse = []() {
+    auto flag = absl::GetFlag(FLAGS_{{ prefix }}{{ value.flag_name }});
+    {{ value.type }} result;
+    for (auto& item : flag) {
+      auto value = core::from_chars<uint16_t>(item);
+      result.emplace_back(value);
+    }
+    return result;
+  };
+  static const {{ value.type }} result{parse()};
+{%- else %}
+  static const {{ value.type }} result{absl::GetFlag(FLAGS_{{ prefix }}{{ value.flag_name }})};
+{%- endif %}
+{%- endif %}
+  return result;
+}
+{% endfor %}
+
+{{ name }}__flags::{{ name }}__flags() :
+{%- for value in values %}
+{%- if not value.is_external %}
+{%- if value.is_string %}
+    {{ value.name }}{ {{ name }}::{{ prefix }}{{ value.name}}() }
+{%- else %}
+    {{ value.name }}{ {{ name }}::{{ prefix }}{{ value.name}}() }
+{%- endif %}
+{%- endif %}
+{%- if not loop.last %}
+    ,
+{%- endif %}
+{%- endfor %}
+{}
+
+{% include 'namespace_end' %}
---
+++ templates/flags/flags.hpp
@@ -0,0 +1,80 @@
+{% include 'autogen' %}
+
+#pragma once
+
+// only needed if any flags have been declared public
+#include <absl/flags/declare.h>
+
+#include <chrono>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "roq/io/web/uri.hpp"
+
+// only needed if any flags have been declared public
+#include "roq/compat.hpp"
+
+#include "roq/core/flags/non_empty.hpp"
+#include "roq/core/flags/non_zero.hpp"
+
+{% for value in values %}
+{%- if value.is_public %}
+ABSL_DECLARE_FLAG({{ value.validator }}, {{ value.name }}) ROQ_PUBLIC;
+{%- endif %}
+{%- endfor %}
+
+{% include 'namespace_begin' %}
+
+struct {{ name }} final {
+{% for value in values %}
+{%- if not value.is_external %}
+{%- if value.is_string %}
+  static std::string_view {{ prefix }}{{ value.name }}();
+{%- else %}
+{%- if value.is_pod_or_std %}
+  static {{ value.type }} {{ prefix }}{{ value.name }}();
+{%- else %}
+  static const {{ value.type }} &{{ prefix }}{{ value.name }}();
+{%- endif %}
+{%- endif %}
+{%- endif %}
+{%- endfor %}
+  // external
+{%- for value in values %}
+{%- if value.is_external %}
+{%- if value.is_string %}
+  static std::string_view {{ prefix }}{{ value.name }}();
+{%- else %}
+{%- if value.is_pod_or_std %}
+  static {{ value.type }} {{ prefix }}{{ value.name }}();
+{%- else %}
+  static const {{ value.type }} &{{ prefix }}{{ value.name }}();
+{%- endif %}
+{%- endif %}
+{%- endif %}
+{%- endfor %}
+};
+
+struct {{ name }}__flags final {
+  {{ name }}__flags();
+
+{% for value in values %}
+{%- if not value.is_external %}
+{%- if value.is_string %}
+  std::string_view const {{ value.name }};
+{%- else %}
+{%- if value.is_pod_or_std %}
+  {{ value.type }} const {{ value.name }};
+{%- else %}
+  {{ value.type }} const& {{ value.name }};
+{%- endif %}
+{%- endif %}
+{%- endif %}
+{%- endfor %}
+};
+
+{% include 'namespace_end' %}
+
+{% include 'format2' %}
