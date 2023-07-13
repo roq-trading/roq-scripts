edadf406177e6c9c79619f7bf4a48451af11a497
thraneh <thraneh@gmail.com>
2023-07-13 11:00:18 +0200

    support size_t

--- autogen.py
+++ autogen.py
@@ -39,10 +39,12 @@ defaults = {
     "uint32_t": "{}",
     "int64_t": "{}",
     "uint64_t": "{}",
+    "size_t": "{}",
     "std::int32_t": "{}",
     "std::uint32_t": "{}",
     "std::int64_t": "{}",
     "std::uint64_t": "{}",
+    "std::size_t": "{}",
     "double": "std::numeric_limits<double>::quiet_NaN()",
     "std::chrono::year_month_day": "{}",
     "std::chrono::hh_mm_ss<std::chrono::milliseconds>": "{}",
@@ -53,8 +55,10 @@ unsigned = {
     "uint16_t",
     "uint32_t",
     "uint64_t",
+    "size_t",
     "std::uint32_t",
     "std::uint64_t",
+    "std::size_t",
 }
 
 chrono = {
