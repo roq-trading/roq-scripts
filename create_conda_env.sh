1d4ecd9090bc1510f8f42d5d264e9ae622c5d240
thraneh <thraneh@gmail.com>
2023-08-21 08:40:30 +0200

    fix

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -117,7 +117,7 @@ bash "$OPT_DIR/$CONDA_INSTALLER" -b -p "$CONDA_DIR"
 
 echo -e "\033[1;34mUpdate conda...\033[0m"
 
-conda update --name base --channel conda-forge --yes conda
+"$CONDA_DIR/bin/conda" update --name base --channel conda-forge --yes conda
 
 # note! mamba: default timeout seems too short
 # reference:
