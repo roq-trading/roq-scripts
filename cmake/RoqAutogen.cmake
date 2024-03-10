5fdfe1341732632dd058145007c2e39441d7f4df
thraneh <thraneh@gmail.com>
2024-03-10 13:44:44 +0100

    rst doc for flags

--- cmake/RoqAutogen.cmake
+++ cmake/RoqAutogen.cmake
@@ -1,5 +1,41 @@
 set(AUTOGEN_PY "${CMAKE_SOURCE_DIR}/scripts/autogen.py")
 
+function(roq_autodoc)
+  set(options DUMMY_OPTION)
+  set(one_value OUTPUT NAMESPACE OUTPUT_TYPE FILE_TYPE SCHEMA_DIR)
+  set(multi_value SOURCES DEPENDS)
+  cmake_parse_arguments(
+    SELF
+    "${options}"
+    "${one_value}"
+    "${multi_value}"
+    ${ARGN}
+  )
+  unset(result)
+  foreach(source ${SELF_SOURCES})
+    get_filename_component(dir ${source} DIRECTORY)
+    get_filename_component(name ${source} NAME_WE)
+    set(target "${CMAKE_CURRENT_BINARY_DIR}/${dir}/${name}.${SELF_FILE_TYPE}")
+    if(DEFINED SELF_SCHEMA_DIR)
+      set(real_source "${SELF_SCHEMA_DIR}/${source}")
+    else()
+      set(real_source "${CMAKE_CURRENT_SOURCE_DIR}/${source}")
+    endif()
+    set(depends "${Python3_EXECUTABLE}" "${AUTOGEN_PY}" "${real_source}")
+    if(SELF_DEPENDS)
+      list(APPEND depends ${SELF_DEPENDS})
+    endif(SELF_DEPENDS)
+    add_custom_command(
+      OUTPUT "${target}"
+      COMMAND ${Python3_EXECUTABLE} "${AUTOGEN_PY}" --namespace "${SELF_NAMESPACE}" --output-type "${SELF_OUTPUT_TYPE}" --file-type "${SELF_FILE_TYPE}" "${real_source}" > "${target}"
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${dir}"
+      VERBATIM
+      DEPENDS "${depends}")
+    list(APPEND result "${target}")
+  endforeach()
+  set(${SELF_OUTPUT} ${result} PARENT_SCOPE)
+endfunction()
+
 function(roq_autogen)
   set(options DUMMY_OPTION)
   set(one_value OUTPUT NAMESPACE OUTPUT_TYPE FILE_TYPE)
